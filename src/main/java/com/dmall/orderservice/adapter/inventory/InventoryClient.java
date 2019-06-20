2ab7b459871ae6834673c91be584f0a3ac59d2d2
hazel <hazel.hua@oocl.com>
2019-06-20 16:23:05 +0800

    hystrix

--- src/main/java/com/dmall/orderservice/adapter/inventory/InventoryClient.java
+++ src/main/java/com/dmall/orderservice/adapter/inventory/InventoryClient.java
@@ -1,12 +1,19 @@
 package com.dmall.orderservice.adapter.inventory;
 
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 //TODO: add a fallback for InventoryClient
-@FeignClient(value = "inventory-service")
+
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@FeignClient(value = "inventory-service",fallback = InventoryClient.class)
 public interface InventoryClient {
     @RequestMapping(method = RequestMethod.POST, value = "/inventories/lock")
     String lock(Lock lock);
