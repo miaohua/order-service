98157c96dd66ac9a98c2a4ad99985f5368446c59
hazel <hazel.hua@oocl.com>
2019-06-20 15:08:51 +0800

    configure Eurake service

--- src/main/java/com/dmall/orderservice/OrderServiceApplication.java
+++ src/main/java/com/dmall/orderservice/OrderServiceApplication.java
@@ -2,10 +2,12 @@ package com.dmall.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableFeignClients
 //TODO: enable feign client
 public class OrderServiceApplication {
 
